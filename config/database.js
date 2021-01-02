49e3ebbe4d6641e4d4732552ad533f2710fd49d6
Jerry Hsieh <50159290+jh2k2@users.noreply.github.com>
2021-01-02 07:35:23 -0800

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-    database: 'mongodb+srv://homefetchadmin:2oq20XFHpRw2ASzk@cluster0.8ub2o.mongodb.net/Cluster0?retryWrites=true&w=majority',
+    database: '',
     secret: 'mojatajnadsd'
 }
