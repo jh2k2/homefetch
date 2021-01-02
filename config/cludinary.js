fed304342590e0d00348bbcebb14e2d1d73fc1a9
Jerry Hsieh <50159290+jh2k2@users.noreply.github.com>
2021-01-02 07:35:43 -0800

    Update cludinary.js

--- config/cludinary.js
+++ config/cludinary.js
@@ -1,9 +1,9 @@
 const cloudinary = require('cloudinary');
 
 cloudinary.config({
-    cloud_name: 'df08ciyv5',
-    api_key: '358533746117916',
-    api_secret: 'xulQZPMh3Es_MZpu_CiKUpPqNi4'
+    cloud_name: '',
+    api_key: '',
+    api_secret: ''
 });
 
 module.exports = cloudinary;
